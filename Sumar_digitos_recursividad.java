36b4953856284c7c6ed1abeb127628bee22bcecb
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-03-05 02:04:59 -0300

    Update Sumar_digitos_recursividad.java

--- Sumar_digitos_recursividad.java
+++ Sumar_digitos_recursividad.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class Main {
+  
+  public static void main(String [] args){
+     /*Author Nordevelop
+     * Sumar los digitos de un numero con recursividad
+     */
+    Scanner entra=new Scanner(System.in);
+    System.out.println("Ingrese un numero para sumar sus digitos:  ");
+       numero.nextint();
+    System.out.println("la suma de los digitos es: "+ Sumadigitos(numero));
+  }
+  public static int Sumadigitos(int numero){
+     if(numero <10)
+        return numero;
+     else
+        return (numero % 10) + Sumadigitos(numero/10);
+   }
+}
